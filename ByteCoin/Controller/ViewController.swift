ab27c7b4aa21a98d97a631c4a16e1239b277af15
danny <bandofcs@gmail.com>
2021-08-21 18:01:22 +0800

    Delegate methods updated

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -31,7 +31,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
     }
-
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //print(coinManager.currencyArray[row])
+        coinManager.getCoinPrice(for: coinManager.currencyArray[row])
+    }
 
 }
 
